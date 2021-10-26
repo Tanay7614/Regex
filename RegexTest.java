11e0a1c4cde129a5829f0a080af635315872b093
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-26 16:25:16 +0530

    First name pattrn

--- RegexTest.java
+++ RegexTest.java
@@ -8,16 +8,12 @@ import java.util.regex.Pattern;
 public class RegexTest {
         
 
-		public void validateFirstName(String name) {
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+		 public void validateFirstName(String name)
+		 {
+		 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
         	}
-		public void validateLastName(String name) {
-			 String regex="[A-Z]{1}[a-z]{2,8}";
-			 Pattern pattern=Pattern.compile(regex);
-        	 Matcher matcher= pattern.matcher(name);
-        	 System.out.println("String matches last name "+matcher.matches());
-		}
+		
 }
