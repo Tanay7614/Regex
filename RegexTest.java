81a26c95a88bb34d9202a91d3999b655c2347643
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-26 17:02:49 +0530

    Last name pattern

--- RegexTest.java
+++ RegexTest.java
@@ -6,18 +6,18 @@ import java.util.regex.Pattern;
 
 
 public class RegexTest {
-        
-
-		public void validateFirstName(String name) {
+        public void validateFirstName(String name) 
+	{
 			 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        	}
-		public void validateLastName(String name) {
-			 String regex="[A-Z]{1}[a-z]{2,8}";
-			 Pattern pattern=Pattern.compile(regex);
-        	 Matcher matcher= pattern.matcher(name);
-        	 System.out.println("String matches last name "+matcher.matches());
-		}
+        }
+	public void validateLastName(String name) 
+	{
+	   String regex="[A-Z]{1}[a-z]{2,8}";
+	   Pattern pattern=Pattern.compile(regex);
+           Matcher matcher= pattern.matcher(name);
+           System.out.println("String matches last name "+matcher.matches());
+	}
 }
