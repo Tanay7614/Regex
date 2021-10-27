4bd987903e89f4b552986971f6bbe280489adda4
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-27 20:55:29 +0530

    Pattern for mobile no

--- RegexTest.java
+++ RegexTest.java
@@ -5,7 +5,8 @@ import java.util.regex.Pattern;
 
 
 
-public class RegexTest {
+public class RegexTest 
+{
         
 
 		public void validateFirstName(String name)
@@ -14,27 +15,27 @@ public class RegexTest {
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+                }
 		
 		public void validateLastName(String name)
 		{
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+                 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+                }
 		
 		public void validateEmail(String name)
 		{
-	     String regex= " ^[A-Za-z0-9+_.-]+@(.+)$";
-       	 Pattern pattern=Pattern.compile(regex);
-       	 Matcher matcher= pattern.matcher(name);
-       	 System.out.println("String matches "+matcher.matches());
-       	}
+	        String regex= " ^[A-Za-z0-9+_.-]+@(.+)$";
+       	        Pattern pattern=Pattern.compile(regex);
+         	Matcher matcher= pattern.matcher(name);
+       	        System.out.println("String matches "+matcher.matches());
+          	}
 		
 		public void validateMobileNo(String name) 
 		{
-			 String regex= "[9]{1}[1]{1}[\s]+[0-9]{10}";
+		 String regex= "[9]{1}[1]{1}[\s]+[0-9]{10}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(name);
 	       	 System.out.println("String matches "+matcher.matches());
