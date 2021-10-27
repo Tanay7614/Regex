2ca3f2d9c41dd6d52f3c1cd5e73a0ede8b9a4173
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-27 21:03:39 +0530

    Pattern for password

--- RegexTest.java
+++ RegexTest.java
@@ -10,31 +10,31 @@ public class RegexTest {
 
 		public void validateFirstName(String name)
 		{
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+		 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+                }
 		
 		public void validateLastName(String name)
 		{
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+		 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+                }
 		
 		public void validateEmail(String name)
 		{
-	     String regex= " ^[A-Za-z0-9+_.-]+@(.+)$";
-       	 Pattern pattern=Pattern.compile(regex);
-       	 Matcher matcher= pattern.matcher(name);
-       	 System.out.println("String matches "+matcher.matches());
-       	}
+	          String regex= " ^[A-Za-z0-9+_.-]+@(.+)$";
+       	          Pattern pattern=Pattern.compile(regex);
+      	       	  Matcher matcher= pattern.matcher(name);
+       		  System.out.println("String matches "+matcher.matches());
+       		}
 		
 		public void validateMobileNo(String name) 
 		{
-			 String regex= "[9]{1}[1]{1}[\s]+[0-9]{10}";
+		 String regex= "[9]{1}[1]{1}[\s]+[0-9]{10}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(name);
 	       	 System.out.println("String matches "+matcher.matches());
@@ -43,7 +43,7 @@ public class RegexTest {
 
 		public void password(String name)
 		{
-			 String regex= "[a-zA-Z]{4,8}";
+		 String regex= "[a-zA-Z]{4,8}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(name);
 	       	 System.out.println("String matches "+matcher.matches());
