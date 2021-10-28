575d8d7dc3b2e30df78ffa3cd75a76d53d730b31
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-28 19:30:39 +0530

    Exactly 1 special character

--- RegexTest.java
+++ RegexTest.java
@@ -8,27 +8,27 @@ public class RegexTest {
 
 		public void validateFirstName(String name)
 		{
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+		 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+     		 }
 		
 		public void validateLastName(String name)
 		{
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+		 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+    	        }
 		
 		public void validateEmail(String name)
 		{
-	     String regex= " ^[A-Za-z0-9+_.-]+@(.+)";
-       	 Pattern pattern=Pattern.compile(regex);
-       	 Matcher matcher= pattern.matcher(name);
-       	 System.out.println("String matches "+matcher.matches());
-       	}
+	 	 String regex= " ^[A-Za-z0-9+_.-]+@(.+)";
+      	 	 Pattern pattern=Pattern.compile(regex);
+		 Matcher matcher= pattern.matcher(name);
+      	 	 System.out.println("String matches "+matcher.matches());
+    	   	}
 		
 		public void validateMobileNo(String name) 
 		{
@@ -50,15 +50,16 @@ public class RegexTest {
 
 		public void passwordAtleast1capitalLetter(String pass)
 		{
-			 String regex= "(?=[A-Z])[a-zA-Z0-9]{8,}";
+                 String regex= "(?=[A-Z])[a-zA-Z0-9]{8,}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(pass);
 	       	 System.out.println("String matches "+matcher.matches());
 			
 		}
 
-		public void passwordAtleast1_uc_and_1_numeric_exactly1_spe_ch(String pass) {
-			 String regex= "((?=.*[0-9])(?=.*[A-Z])(?=.*[@#$]).{8,})";
+		public void passwordAtleast1_uc_and_1_numeric_exactly1_spe_ch(String pass)
+		{
+		 String regex= "((?=.*[0-9])(?=.*[A-Z])(?=.*[@#$]).{8,})";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(pass);
 	       	 System.out.println("String matches "+matcher.matches());
