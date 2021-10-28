0389adf50c863dd730c9d203b947969b57a330ce
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-28 19:00:06 +0530

    At least one digit and one character

--- RegexTest.java
+++ RegexTest.java
@@ -8,31 +8,31 @@ public class RegexTest {
 
 		public void validateFirstName(String name)
 		{
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+	         String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+      	        }
 		
 		public void validateLastName(String name)
 		{
-			 String regex="[A-Z]{1}[a-z]{2,8}";
+		 String regex="[A-Z]{1}[a-z]{2,8}";
         	 Pattern pattern=Pattern.compile(regex);
         	 Matcher matcher= pattern.matcher(name);
         	 System.out.println("String matches "+matcher.matches());
-        }
+     	        }
 		
 		public void validateEmail(String name)
 		{
-	     String regex= " ^[A-Za-z0-9+_.-]+@(.+)";
-       	 Pattern pattern=Pattern.compile(regex);
-       	 Matcher matcher= pattern.matcher(name);
-       	 System.out.println("String matches "+matcher.matches());
-       	}
+	     	String regex= " ^[A-Za-z0-9+_.-]+@(.+)";
+       	        Pattern pattern=Pattern.compile(regex);
+       		Matcher matcher= pattern.matcher(name);
+       	        System.out.println("String matches "+matcher.matches());
+       		}
 		
 		public void validateMobileNo(String name) 
 		{
-			 String regex= "[9]{1}[1]{1}[\s]+[0-9]{10}";
+		 String regex= "[9]{1}[1]{1}[\s]+[0-9]{10}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(name);
 	       	 System.out.println("String matches "+matcher.matches());
@@ -41,7 +41,7 @@ public class RegexTest {
 
 		public void password(String name)
 		{
-			 String regex= "[a-zA-Z]{4,8}";
+	         String regex= "[a-zA-Z]{4,8}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(name);
 	       	 System.out.println("String matches "+matcher.matches());
@@ -50,7 +50,7 @@ public class RegexTest {
 
 		public void passwordAtleast1capitalLetter(String pass)
 		{
-			 String regex= "(?=[A-Z])[a-zA-Z0-9]{8,}";
+		 String regex= "(?=[A-Z])[a-zA-Z0-9]{8,}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(pass);
 	       	 System.out.println("String matches "+matcher.matches());
@@ -58,7 +58,7 @@ public class RegexTest {
 		}
 
 		public void passwordAtleast1_uc_and_1_numeric(String pass) {
-			 String regex= "(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8,}";
+		 String regex= "(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8,}";
 	       	 Pattern pattern=Pattern.compile(regex);
 	       	 Matcher matcher= pattern.matcher(pass);
 	       	 System.out.println("String matches "+matcher.matches());
