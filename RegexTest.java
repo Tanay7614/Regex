df140363af262ada8283736bc8cf50e60ce9cf59
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-28 23:06:58 +0530

    email bl

--- RegexTest.java
+++ RegexTest.java
@@ -8,16 +8,26 @@ import java.util.regex.Pattern;
 public class RegexTest {
         
 
-		public void validateFirstName(String name) {
+		public void validateFirstName(String name)
+		{
 			 String regex="[A-Z]{1}[a-z]{2,8}";
-        	 Pattern pattern=Pattern.compile(regex);
-        	 Matcher matcher= pattern.matcher(name);
-        	 System.out.println("String matches "+matcher.matches());
-        	}
-		public void validateLastName(String name) {
+        		 Pattern pattern=Pattern.compile(regex);
+        		 Matcher matcher= pattern.matcher(name);
+        		 System.out.println("String matches "+matcher.matches());
+                }
+		public void validateLastName(String name)
+		{
 			 String regex="[A-Z]{1}[a-z]{2,8}";
 			 Pattern pattern=Pattern.compile(regex);
-        	 Matcher matcher= pattern.matcher(name);
-        	 System.out.println("String matches last name "+matcher.matches());
+        		 Matcher matcher= pattern.matcher(name);
+        		 System.out.println("String matches last name "+matcher.matches());
+		}
+		public void Email(String email)
+		{
+			 String regex= "([a]{1}+)([b]{1}+)([c]{1}+)(\\.[a-z]+)@([b]{1}+)([l]{1}+)((\\.[c]{1}+)([o]{1}))(\\.[a-z]{2})?$";
+			 Pattern pattern=Pattern.compile(regex);
+			 Matcher matcher= pattern.matcher(email);
+	       		 System.out.println("String matches "+matcher.matches());
+			
 		}
 }
